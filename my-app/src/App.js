93b2913ada8c222be3efa955c8fabb86bbd87f18
Khandaker Shayel <kshayel1717@bths.edu>
2022-02-17 20:12:04 -0500

    updated layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,17 +4,17 @@ import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupsPage from './pages/NewMeetups';
 import MainNavigation from './components/layouts/MainNavigation';
+import Layout from './components/layouts/layouts/Layout';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path='/' element={<AllMeetupsPage />} />
         <Route path='/favorites' element={<FavoritesPage />} />
         <Route path='/new-meetup' element={<NewMeetupsPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
