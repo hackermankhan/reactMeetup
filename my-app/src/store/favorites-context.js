0bf018e50d04e35be7d4187532350f113b9e4f9a
Khandaker Shayel <kshayel1717@bths.edu>
2022-02-20 20:32:55 -0500

    local storage functions

--- my-app/src/store/favorites-context.js
+++ my-app/src/store/favorites-context.js
@@ -1,5 +1,25 @@
 import { createContext, useState } from "react";
 
+//local storage functions
+function setLocalStorage(key, value) {
+    try {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (e) {
+        // catch possible errors:
+        // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+    }
+}
+
+function getLocalStorage(key, initialValue) {
+    try {
+        const value = window.localStorage.getItem(key);
+        return value ? JSON.parse(value) : initialValue;
+    } catch (e) {
+        // if error, return initial value
+        return initialValue;
+    }
+}
+
 //take initial value as anything, we chose an object
 const FavoritesContext = createContext({
     favorites: [],
