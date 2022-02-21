d9689580c7912c1d9a76e3fd53da7abaafb11434
Khandaker Shayel <kshayel1717@bths.edu>
2022-02-20 20:36:31 -0500

    Update favorites-context.js

--- my-app/src/store/favorites-context.js
+++ my-app/src/store/favorites-context.js
@@ -5,8 +5,7 @@ function setLocalStorage(key, value) {
     try {
         window.localStorage.setItem(key, JSON.stringify(value));
     } catch (e) {
-        // catch possible errors:
-        // https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+        // catch possible errors
     }
 }
 
